64d6d6ee1c5ebbc2319f9ecd73cebd8166ee9d98
kunndnn <44980639+kunndnn@users.noreply.github.com>
2024-11-21 23:52:24 +0530

    Router optimized

--- src/Router.js
+++ src/Router.js
@@ -18,87 +18,61 @@ const ChatBot = React.lazy(() => import("./Components/ChatBot"));
 const DropzoneComponent = React.lazy(() =>
   import("./Components/DropzoneComponent")
 );
-
 const Weather = React.lazy(() => import("./Components/Weather/Weather"));
 const ImageConverter = React.lazy(() => import("./Components/ImageConverter"));
+
+const routesArray = [
+  {
+    path: "/loader",
+    component: <Loader />,
+  },
+  {
+    path: "/file-to-buffer",
+    component: <FileToBuffer />,
+  },
+  {
+    path: "/bar-code",
+    component: <BarCodeGenerator />,
+  },
+  {
+    path: "/qr-code",
+    component: <QRCodeGenerator />,
+  },
+  {
+    path: "/text-to-speech",
+    component: <TextToSpeech />,
+  },
+  {
+    path: "/live-camera",
+    component: <LiveCamera />,
+  },
+  {
+    path: "/drop-zone",
+    component: <DropzoneComponent />,
+  },
+  {
+    path: "/weather",
+    component: <Weather />,
+  },
+  {
+    path: "/image-converter",
+    component: <ImageConverter />,
+  },
+];
+
+const routes = routesArray.map(({ path, component }) => (
+  <Route
+    path={path}
+    element={<Suspense fallback={<Loading />}>{component}</Suspense>}
+  />
+));
 function Router() {
   return (
     <HashRouter>
       {/* <BrowserRouter> */}
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route
-            path="/loader"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Loader />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/file-to-buffer"
-            element={
-              <Suspense fallback={<Loading />}>
-                <FileToBuffer />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/bar-code"
-            element={
-              <Suspense fallback={<Loading />}>
-                <BarCodeGenerator />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/qr-code"
-            element={
-              <Suspense fallback={<Loading />}>
-                <QRCodeGenerator />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/text-to-speech"
-            element={
-              <Suspense fallback={<Loading />}>
-                <TextToSpeech />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/live-camera"
-            element={
-              <Suspense fallback={<Loading />}>
-                <LiveCamera />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/drop-zone"
-            element={
-              <Suspense fallback={<Loading />}>
-                <DropzoneComponent />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/weather"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Weather />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/image-converter"
-            element={
-              <Suspense fallback={<Loading />}>
-                <ImageConverter />
-              </Suspense>
-            }
-          />
+          {routes}
           {/* <Route
               path="/chat-bot"
               element={
